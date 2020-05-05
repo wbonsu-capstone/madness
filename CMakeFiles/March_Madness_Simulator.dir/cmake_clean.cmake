6db91bc385f2c815408ecd08bd2a09b71529bef8
Wilmot Bonsu <spam.wilmot@gmail.com>
2020-05-05 02:54:36 -0500

    first commit

---
+++ CMakeFiles/March_Madness_Simulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/March_Madness_Simulator.dir/Simulator.cpp.o"
+  "CMakeFiles/March_Madness_Simulator.dir/Team.cpp.o"
+  "CMakeFiles/March_Madness_Simulator.dir/Game.cpp.o"
+  "March_Madness_Simulator.pdb"
+  "March_Madness_Simulator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/March_Madness_Simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
